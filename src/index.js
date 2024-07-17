58f29104201a65064e1a7825ff36e250874a1faa
Minal <minal.pariyar@patancollege.edu.np>
2024-07-17 21:41:36 +0545

    Error handled successfully

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
-import dotenv from 'dotenv';
-import connectDb from './db/connection.js';
+import dotenv from "dotenv";
+import { app } from "./app.js";
+import connectDb from "./db/connection.js";
 dotenv.config({
-    path:'./env'
-})
+  path: "./env",
+});
+const PORT = process.env.PORT || 8000;
 connectDb()
+  .then(() => {
+    app.listen(
+      (PORT,
+      () => {
+        console.log("Server is running in port", PORT);
+      })
+    );
+  })
+  .catch((err) =>
+    app.on("error", () => {
+      console.log("Internal Server Error", err);
+      throw err;
+    })
+  );
---
+++ src/utils/ApiError.js
@@ -0,0 +1,21 @@
+class ApiError extends Error {
+  constructor(
+    statusCode,
+    message = "Something went wrong!!",
+    errors = [],
+    statck = ""
+  ) {
+    super(message);
+    (this.statusCode = statusCode),
+      (this.data = null),
+      (this.message = message),
+      (this.success = true),
+      (this.errors = errors);
+      if(statck){
+        this.stack=statck
+      }else{
+        error.captureStackTrace(this,this.constructor)
+      }
+  }
+}
+export {ApiError}
