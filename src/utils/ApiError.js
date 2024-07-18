e433f6fc59272dc6204f3b2355a92c3118ceb7d3
Minal <minal.pariyar@patancollege.edu.np>
2024-07-18 14:39:51 +0545

    cloudinary uploaded successfully

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -3,7 +3,7 @@ class ApiError extends Error {
     statusCode,
     message = "Something went wrong!!",
     errors = [],
-    statck = ""
+    stack = ""
   ) {
     super(message);
     (this.statusCode = statusCode),
@@ -11,8 +11,8 @@ class ApiError extends Error {
       (this.message = message),
       (this.success = true),
       (this.errors = errors);
-      if(statck){
-        this.stack=statck
+      if(stack){
+        this.stack=stack
       }else{
         error.captureStackTrace(this,this.constructor)
       }
---
+++ src/utils/fleUpload.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs';
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_NAME, 
+    api_key: CLOUDINARY_API_KEY, 
+    api_secret: CLOUDINARY_API_SECRET 
+})
+
+const upload=async(localpath)=>{
+    try{
+        if(!localpath){
+            return null
+        }
+       const response =await cloudinary.uploader.upload(localpath,{
+            resource_type:"auto"
+        })
+        //file has been uploaded
+        console.log("file is uploaded on cloudinary!!",response.url)
+        return response
+       
+    }catch(error){
+        fs.unlinkSync(localpath)
+        return null
+
+    }
+}
+export {upload}
