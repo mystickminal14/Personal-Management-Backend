f7478ba65e971c8401023a4f152e3164db0d55c1
Minal <minal.pariyar@patancollege.edu.np>
2024-07-18 14:00:10 +0545

    implemeneted json web token

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 const userSchema = new mongoose.Schema(
   {
     fullName: {
@@ -63,7 +64,33 @@ userSchema.pre("save", async function (next) {
   this.password = bcrypt.hash(this.password, 10);
   next();
 });
-userSchema.methods.isPasswordCorrect=async function (password){
-   return await bcrypt.compare(password,this.password)
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+userSchema.methods.generateAcsessToken = function () {
+  return  jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+userSchema.methods.generateRefreshToken=function(){
+    return  jwt.sign(
+        {
+          _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+          expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+        }
+      );
 }
 export const User = new mongoose.Schema("User", userSchema);
