54ce8323256e34823054cbe7c5f2e6ccf71dd7e5
Minal <minal.pariyar@patancollege.edu.np>
2024-07-24 09:04:06 +0545

    logout middleware and controller function successfully implemented

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -78,4 +78,24 @@ const login = asyncHandler(async (req, res) => {
   return res.status(200).json(new ApiResponse(200, { user: loggedInUser, refreshToken, accessToken }, "Login Successful!!"));
 });
 
-export { store, login };
+const logOut=asyncHandler(async(req,res)=>{
+  await User.findByIdAndUpdate(req.user._id,
+    {
+      $set:{refreshToken:undefined}
+    },
+    {
+      new:true
+    }
+  )
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+  return res.status(200).
+  clearCookie("accessToken",options).
+  clearCookie("refreshToken",options)
+  .json(new ApiResponse(200,{},"User Logged out Successfully!!"))
+
+})
+
+export { store, login,logOut };
--- src/router/users.routes.js
+++ src/router/users.routes.js
@@ -1,9 +1,13 @@
 import express from 'express';
-import { store,login } from '../controllers/users.controller.js';
+import { store,login,logOut } from '../controllers/users.controller.js';
 import { upload } from '../middleware/multer.middleware.js';
+import { verifyJWT } from '../middleware/auth.middleware.js';
 const router = express.Router();
 router.route("/register").post(
     upload.single('avatar'),
     store);
 router.route("/login").post(login)
+
+//PROTECTED
+router.route("/logout").post(verifyJWT,logOut)
 export { router };
